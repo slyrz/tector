a393ab4d60de6978756a820289e4131901c8d408
slyrz <slyrz@users.noreply.github.com>
2014-10-29 18:33:11 +0100

    use preprocessor only

--- src/core/log.h
+++ src/core/log.h
@@ -3,22 +3,13 @@
 
 #include <stdio.h>
 
-enum level {
-  LOG_DEBUG,
-  LOG_INFO,
-  LOG_WARNING,
-  LOG_ERROR,
-};
-
-static const char *levels[] = {
-  [LOG_DEBUG]   = "\x1b[34;1m" "dbg" "\x1b[0m",
-  [LOG_INFO]    = "\x1b[32;1m" "inf" "\x1b[0m",
-  [LOG_WARNING] = "\x1b[33;1m" "wrn" "\x1b[0m",
-  [LOG_ERROR]   = "\x1b[31;1m" "err" "\x1b[0m",
-};
+#define LOG_DEBUG   "\x1b[34;1m" "dbg" "\x1b[0m"
+#define LOG_INFO    "\x1b[32;1m" "inf" "\x1b[0m"
+#define LOG_WARNING "\x1b[33;1m" "wrn" "\x1b[0m"
+#define LOG_ERROR   "\x1b[31;1m" "err" "\x1b[0m"
 
 #define log_print(level,msg,...) \
-  fprintf (stderr, " %s ~ " msg "\n", levels[level], ##__VA_ARGS__)
+  fprintf (stderr, " " level " ~ " msg "\n", ##__VA_ARGS__)
 
 #define debug(...)      log_print (LOG_DEBUG, __VA_ARGS__)
 #define info(...)       log_print (LOG_INFO, __VA_ARGS__)
