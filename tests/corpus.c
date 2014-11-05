a75f6fa2f936dbe07cff051c2dbd986e1b87c82e
slyrz <slyrz@users.noreply.github.com>
2014-11-05 17:43:55 +0100

    add tests for corpus and scanner

---
+++ tests/corpus.c
@@ -0,0 +1,59 @@
+#include "../src/core/vocab.h"
+#include "../src/core/corpus.h"
+#include "../src/core/serialize.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+const size_t words[] = {
+  1, 0,
+  1, 3,
+  1, 2,
+  1, 1,
+  4, 2, 2, 0, 1,
+  2, 0, 0,
+  1, 3,
+  2, 2, 0,
+  5, 3, 1, 3, 2, 3,
+  1, 2,
+  3, 3, 0, 1,
+  3, 3, 3, 2,
+  2, 2, 1,
+  2, 3, 0,
+  2, 2, 1,
+  1, 0,
+  1, 1,
+  4, 1, 0, 2, 2,
+  1, 1,
+  1, 2,
+};
+
+int
+main (void)
+{
+  struct vocab *v;
+  struct corpus *c;
+  size_t i;
+  size_t j;
+  size_t k;
+
+  v = vocab_new ();
+  assert (v != NULL);
+  assert (vocab_add (v, "cat") == 0);
+  assert (vocab_add (v, "dog") == 0);
+  assert (vocab_add (v, "frog") == 0);
+  assert (vocab_add (v, "mous") == 0);
+  c = corpus_new (v);
+  assert (c != NULL);
+  assert (corpus_parse (c, "tests/testdata/corpus.txt") == 0);
+  assert (memcmp (c->words.ptr, words, c->words.len * sizeof (size_t)) == 0);
+  for (i = j = 0; i < c->sentences.len; i++) {
+    assert (c->sentences.ptr[i]->len == c->words.ptr[j++]);
+    for (k = 0; k < c->sentences.ptr[i]->len; k++)
+      assert (c->sentences.ptr[i]->words[k] == c->words.ptr[j++]);
+  }
+  corpus_free (c);
+  vocab_free (v);
+  return EXIT_SUCCESS;
+}
---
+++ tests/scanner.c
@@ -0,0 +1,41 @@
+#include "../src/core/scanner.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+const char *lines[] = {
+  "this is sentence one",
+  "this is sentence two",
+  "this is sentence three",
+  "this is sentence four",
+  "this is sentence five",
+};
+
+static void
+test (const char *path)
+{
+  struct scanner *s;
+  char b[1024];
+  int i;
+
+  s = scanner_new (path);
+  assert (s != NULL);
+  i = 0;
+  while (scanner_readline (s, b, sizeof (b)) == 0)
+    assert ((i < 5) && (strcmp (lines[i++], b) == 0));
+  scanner_rewind (s);
+  i = 0;
+  while (scanner_readline (s, b, sizeof (b)) == 0)
+    assert ((i < 5) && (strcmp (lines[i++], b) == 0));
+  scanner_free (s);
+}
+
+int
+main (void)
+{
+  test ("tests/testdata/scanner_clean.txt");
+  test ("tests/testdata/scanner_dirty.txt");
+  test ("tests/testdata/scanner_large.txt");
+  return EXIT_SUCCESS;
+}
