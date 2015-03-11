927b8a664beadbcc45471e1d8a7cc3d0ed4ae2b7
slyrz <slyrz@users.noreply.github.com>
2015-03-12 00:48:50 +0100

    update tests

--- tests/corpus.c
+++ tests/corpus.c
@@ -1,6 +1,5 @@
 #include "../src/core/vocab.h"
 #include "../src/core/corpus.h"
-#include "../src/core/serialize.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -19,12 +18,26 @@ const size_t words[] = {
   4, 1, 0, 2, 2,
 };
 
-static void
-verify (struct corpus *c)
+int
+main (void)
 {
+  struct corpus *c;
+  struct vocab *v;
+
   size_t i;
   size_t j;
   size_t k;
+
+  v = vocab_new ();
+  assert (v != NULL);
+  assert (vocab_add (v, "cat") == 0);
+  assert (vocab_add (v, "dog") == 0);
+  assert (vocab_add (v, "frog") == 0);
+  assert (vocab_add (v, "mouse") == 0);
+
+  c = corpus_new (v);
+  assert (c != NULL);
+  assert (corpus_parse (c, "tests/testdata/corpus.txt") == 0);
 
   assert (memcmp (c->words.ptr, words, c->words.len * sizeof (size_t)) == 0);
   for (i = j = 0; i < c->sentences.len; i++) {
@@ -32,47 +45,6 @@ verify (struct corpus *c)
     for (k = 0; k < c->sentences.ptr[i]->len; k++)
       assert (c->sentences.ptr[i]->words[k] == c->words.ptr[j++]);
   }
-}
-
-int
-main (void)
-{
-  struct corpus *c;
-  struct vocab *v;
-  int i;
-
-  v = vocab_new ();
-  assert (v != NULL);
-  assert (vocab_add (v, "cat") == 0);
-  assert (vocab_add (v, "dog") == 0);
-  assert (vocab_add (v, "frog") == 0);
-  assert (vocab_add (v, "mous") == 0);
-
-  c = corpus_new (v);
-  assert (c != NULL);
-  assert (corpus_parse (c, "tests/testdata/corpus.txt") == 0);
-  verify (c);
-  assert (corpus_save (c, "/tmp/corpus.bin") == 0);
   corpus_free (c);
-
-  c = corpus_new (v);
-  assert (c != NULL);
-  assert (corpus_load (c, "/tmp/corpus.bin") == 0);
-  verify (c);
-  corpus_free (c);
-
-  assert (vocab_add (v, "spider") == 0);
-
-  c = corpus_new (v);
-  assert (c != NULL);
-  assert (corpus_load (c, "/tmp/corpus.bin") != 0);
-  corpus_free (c);
-
-  c = corpus_new (v);
-  for (i = 0; i < 500; i++)
-    assert (corpus_parse (c, "tests/testdata/corpus.txt") == 0);
-  corpus_free (c);
-
-  vocab_free (v);
   return EXIT_SUCCESS;
 }
--- tests/vocab.c
+++ tests/vocab.c
@@ -1,6 +1,4 @@
 #include "../src/core/vocab.h"
-#include "../src/core/serialize.h"
-#include "../src/core/filter.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -25,79 +23,33 @@ static char words[][32] = {
   "turtle",
 };
 
-static char*
-randstr (void)
-{
-  static char b[64 + 1];
-
-  long int r = lrand48 ();
-  int i;
-
-  for (i = 0; i < sizeof (b) - 1; i++) {
-    if (r == 0)
-      r = lrand48 ();
-    b[i] = 'a' + (r % 26); r >>= 1;
-  }
-  b[i] = '\0';
-  return b;
-}
-
 int
 main (void)
 {
   const size_t n = len (words);
-  const size_t x = 100000;
   size_t c[n];
   size_t i;
   size_t j;
   struct vocab *v;
 
-  memset (c, 0, n * sizeof (size_t));
-
-  for (i = 0; i < n; i++)
-    filter (words[i]);
-
   v = vocab_new ();
+  v->min = 0;
   assert (v != NULL);
-  assert (v->len == 0);
   assert (vocab_parse (v, "tests/testdata/vocab.txt") == 0);
-  assert (v->len == n);
-
-  for (i = 0; i < n; i++)
-    v->entries[i].count = 0;
-
-  for (i = 0; i < 10000; i++) {
-    j = (size_t) lrand48 () % n;
-    assert (vocab_add (v, words[j]) == 0);
-    c[j]++;
-  }
-  assert (v->len == n);
-  assert (vocab_shrink (v, 2) == 0);
-  assert (v->len == n);
-
-  for (i = 0; i < x; i++)
-    vocab_add (v, randstr ());
-
-  assert (v->len == x + n);
-  assert (vocab_shrink (v, 2) == 0);
-  assert (v->len == n);
-  assert (vocab_encode (v) == 0);
-
-  for (i = 0; i < n; i++) {
+  assert (v->len == len (words));
+  for (i = 0; i < len (words); i++) {
     assert (vocab_find (v, words[i], &j) == 0);
-    assert (v->entries[j].count == c[i]);
+    c[i] = v->entries[j].count;
   }
   assert (vocab_save (v, "/tmp/vocab.bin") == 0);
   vocab_free (v);
 
-  v = vocab_new ();
+  v = vocab_open ("/tmp/vocab.bin");
   assert (v != NULL);
-  assert (v->len == 0);
-  assert (vocab_load (v, "/tmp/vocab.bin") == 0);
-  assert (v->len == n);
-  for (i = 0; i < n; i++) {
+  assert (v->len == len (words));
+  for (i = 0; i < len (words); i++) {
     assert (vocab_find (v, words[i], &j) == 0);
-    assert (v->entries[j].count == c[i]);
+    assert (c[i] == v->entries[j].count);
   }
   vocab_free (v);
   return EXIT_SUCCESS;
