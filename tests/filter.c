2d212d09e6be4d2047ebc8c110b60bca10886767
slyrz <slyrz@users.noreply.github.com>
2015-03-12 10:47:18 +0100

    shorten test cases

--- tests/filter.c
+++ tests/filter.c
@@ -29,16 +29,7 @@ main (void)
   test ("test test t#e_s!t", "test test");
   test ("i test a am of test we they were t#e_s!t", "test test");
   test ("testing testing testing", "test test test");
-  test ("riverrun, past Eve and Adam's, from swerve of shore to bend "
-        "of bai, brings us by a commodius vicus of recirculation back to "
-        "Howth Castle and Environs.",
-        "riverrun past ev adam swerv shore bend bai bring us commodiu vicu "
-        "recircul back howth castl environ");
-  test ("Sir Tristram, violer d'amores, fr'over the short sea, had "
-        "passencore rearrived from North Armorica on this side the scraggy "
-        "isthmus of Europe Minor to wielderfight his penisolate war:",
-        "sir tristram violer damor frover short sea passencor rearriv "
-        "north armorica side scraggi isthmu europ minor wielderfight "
-        "penisol war");
+  test ("http://www.example.org/", "");
+  test ("www.example.org", "wwwexampleorg");
   return EXIT_SUCCESS;
 }
