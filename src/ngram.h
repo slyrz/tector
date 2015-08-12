ce8048289b31dd47c451d4687bd988cdb3bfcd77
slyrz <slyrz@users.noreply.github.com>
2015-08-12 12:37:54 +0200

    remove unused ngram functions

--- src/ngram.h
+++
@@ -1,7 +0,0 @@
-#ifndef TECTOR_NGRAM_H
-#define TECTOR_NGRAM_H
-
-void bigram (char *dst, const char *a, const char *b);
-void trigram (char *dst, const char *a, const char *b, const char *c);
-
-#endif
