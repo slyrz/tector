ef2a1bc966b98204e2b8836fb3f89375bee6aa4e
slyrz <slyrz@users.noreply.github.com>
2015-08-14 00:12:15 +0200

    compare all fields of the vocab entries in test case

--- tests/vocab.c
+++ tests/vocab.c
@@ -8,49 +8,53 @@
 #define len(x) \
   (sizeof (x) / sizeof (x[0]))
 
-static char words[][32] = {
-  "baboon",
-  "bear",
-  "cat",
-  "dog",
-  "donkey",
-  "frog",
-  "giraffe",
-  "mole",
-  "mouse",
-  "octopus",
-  "rabbit",
-  "turtle",
+struct test_vector {
+  char *word;
+  uint64_t count;
+  uint64_t code;
+  uint32_t point[5];
 };
+
+/**INDENT-OFF**/
+static struct test_vector test_vectors[] = {
+  { "mouse",   58, 0x0000000e, {10, 8, 5, 0, 0} },
+  { "rabbit",  51, 0x0000000a, {10, 8, 5, 0, 0} },
+  { "octopus", 46, 0x0000000c, {10, 8, 4, 0, 0} },
+  { "dog",     45, 0x00000008, {10, 8, 4, 0, 0} },
+  { "giraffe", 45, 0x0000001f, {10, 9, 7, 3, 0} },
+  { "cat",     43, 0x00000017, {10, 9, 7, 3, 0} },
+  { "frog",    39, 0x0000001b, {10, 9, 7, 2, 0} },
+  { "turtle",  37, 0x00000013, {10, 9, 7, 2, 0} },
+  { "bear",    37, 0x0000001d, {10, 9, 6, 1, 0} },
+  { "baboon",  35, 0x00000015, {10, 9, 6, 1, 0} },
+  { "mole",    34, 0x00000019, {10, 9, 6, 0, 0} },
+  { "donkey",  30, 0x00000011, {10, 9, 6, 0, 0} },
+};
+/**INDENT-ON**/
 
 int
 main (void)
 {
-  const size_t n = len (words);
-  size_t c[n];
+  struct vocab *v;
   size_t i;
   size_t j;
-  struct vocab *v;
 
   v = vocab_new ();
   v->min = 0;
   assert (v != NULL);
   assert (vocab_parse (v, "tests/testdata/vocab.txt") == 0);
-  assert (v->len == len (words));
-  for (i = 0; i < len (words); i++) {
-    assert (vocab_find (v, words[i], &j) == 0);
-    c[i] = v->entries[j].count;
-  }
+  assert (v->len == len (test_vectors));
   assert (vocab_save (v, "/tmp/vocab.bin") == 0);
   vocab_free (v);
 
   v = vocab_open ("/tmp/vocab.bin");
-  assert (v != NULL);
-  assert (v->len == len (words));
-  for (i = 0; i < len (words); i++) {
-    assert (vocab_find (v, words[i], &j) == 0);
-    assert (c[i] == v->entries[j].count);
+  for (i = 0; i < len (test_vectors); i++) {
+    assert (strcmp (v->entries[i].word, test_vectors[i].word) == 0);
+    assert (memcmp (v->entries[i].point, test_vectors[i].point, sizeof (test_vectors[i].point)) == 0);
+    assert (v->entries[i].count == test_vectors[i].count);
+    assert (v->entries[i].code == test_vectors[i].code);
   }
   vocab_free (v);
+
   return EXIT_SUCCESS;
 }
